a1bbecce9cec5b332dc24af281cc3855594ff99d
denisboyun <denisboyun@gmail.com>
2012-11-10 18:29:44 +0200

    Done typical face of ecg

--- src/ua/stu/view/fragments/ECGPanelFragment.java
+++ src/ua/stu/view/fragments/ECGPanelFragment.java
@@ -1,6 +1,5 @@
 package ua.stu.view.fragments;
 
-import ua.stu.view.scpview.ECGPanel;
 import ua.stu.view.scpview.R;
 import android.annotation.TargetApi;
 import android.app.Fragment;
@@ -11,8 +10,6 @@ import android.view.ViewGroup;
 
 @TargetApi(11)
 public class ECGPanelFragment extends Fragment {
-	
-	private ECGPanel ecgPanel;
 	
 	@Override
 	  public View onCreateView(LayoutInflater inflater, ViewGroup container,

d18cd80caab9f26618ca0fbf22f332123e00da69
denisboyun <denisboyun@gmail.com>
2012-11-18 23:32:48 +0200

    Second merge

--- src/ua/stu/scplib/data/DataHandler.java
+++ src/ua/stu/scplib/data/DataHandler.java
@@ -1,66 +1,133 @@
 package ua.stu.scplib.data;
 
 import java.io.BufferedInputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import ua.stu.scplib.attribute.BinaryInputStream;
-import ua.stu.scplib.graphic.SCPSourceECG;
-import ua.stu.scplib.graphic.SourceECG;
+import ua.stu.scplib.attribute.GraphicAttribute;
+import ua.stu.scplib.attribute.GraphicAttributeBase;
+import ua.stu.scplib.structure.SCPECG;
 
 public class DataHandler {
-	
-	SourceECG sourceECG = null;
-	int nTilesPerColumn = 0;
-	int nTilesPerRow = 0;
-	float widthOfTileInMilliSeconds = 0;
-	float heightOfTileInMilliVolts = 0;
-	float timeOffsetInMilliSeconds = 0;
-	// image params
-	float horizontalPixelsPerMilliSecond = 0;
-	float milliMetresPerPixel = 0;
-	float verticalPixelsPerMilliVolt = 0;
-	// i don't know, what does it mean
-	boolean truederiveAdditionalLeads = false;
-	boolean fillBackGroundFirst = false;
-	
-	/**
-	 * @param	#1 SCPECG data filename,
-	 *			#2 number of tiles per column
-	 *			#3 the number of tiles per row,
-	 *			#4 time offset in milliseconds for the left edge of the display
-	 * @throws FileNotFoundException 
-	 */
-	DataHandler(String filename) throws FileNotFoundException {
-				sourceECG = null;
-				BinaryInputStream i = null;
-				i = new BinaryInputStream(new BufferedInputStream(new FileInputStream(filename)),false);		// little endian
-				
-				try {
-					sourceECG = new SCPSourceECG(i,truederiveAdditionalLeads);
-				} catch (IOException e) {e.printStackTrace();}
-				
-				// assume screen is 72 dpi aka 72/25.4 pixels/mm
-				milliMetresPerPixel = (float)(25.4/72);
+	private SCPECG scpecg = null;
+	private BinaryInputStream i = null;
+	private GraphicAttributeBase graphic = null;
+	private PInfo pi = null;
+	private OInfo oi = null;
 
-				// ECG's normally printed at 25mm/sec and 10 mm/mV
-				horizontalPixelsPerMilliSecond = 25/(1000*milliMetresPerPixel);
-				
-				float verticalPixelsPerMilliVolt = 10/(milliMetresPerPixel);
-	
-				Object[] panelParams = {
-					sourceECG.getSamples(),
-					sourceECG.getNumberOfChannels(),
-					sourceECG.getNumberOfSamplesPerChannel(),
-					sourceECG.getChannelNames(),
-					nTilesPerColumn,nTilesPerRow,
-					sourceECG.getSamplingIntervalInMilliSeconds(),
-					sourceECG.getAmplitudeScalingFactorInMilliVolts(),
-					horizontalPixelsPerMilliSecond,verticalPixelsPerMilliVolt,
-					timeOffsetInMilliSeconds,
-					sourceECG.getDisplaySequence(),
-					800,400,fillBackGroundFirst
-				};
+	/**
+	 * get patient info from ECG
+	 * 
+	 * @return PInfo
+	 */
+	public PInfo getPInfo() {
+		return pi;
+	}
+
+	/**
+	 * set patient info from ECG
+	 * 
+	 * @return void
+	 * 
+	 * @param PInfo
+	 *            :object which handle patient info
+	 */
+	public void setPInfo(PInfo pi) {
+		this.pi = pi;
+	}
+
+	/**
+	 * get other information from ECG
+	 * 
+	 * @return OInfo
+	 * 
+	 */
+	public OInfo getOInfo() {
+		return oi;
+	}
+
+	/**
+	 * set other information from ECG
+	 * 
+	 * @return void
+	 * 
+	 * @param OInfo
+	 *            :object which handle other info
+	 */
+
+	public void setOInfo(OInfo oi) {
+		this.oi = oi;
+	}
+
+	/**
+	 * <p>
+	 * Open file and create SCPECGG.
+	 * </p>
+	 * 
+	 * @param String
+	 *            filename
+	 */
+	private void openFile(String filename) {
+		// = null;
+		try {
+			i = new BinaryInputStream(new BufferedInputStream(
+					new FileInputStream(new File(filename))), false);
+			scpecg = new SCPECG(i, false);
+			i = new BinaryInputStream(new BufferedInputStream(
+					new FileInputStream(filename)), false);
+			graphic = new GraphicAttribute(i, false);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @param #1 SCPECG data filename,
+	 */
+	public DataHandler(String filename) {
+		openFile(filename);
+		pi = new PInfo(scpecg);
+		oi = new OInfo(scpecg);
+	}
+
+	/**
+	 * Take graphic attributes of ECG
+	 * 
+	 * @return the graphic
+	 */
+	public GraphicAttributeBase getGraphic() {
+		return graphic;
+	}
+
+	public static void main(String arg[]) {
+		System.out.println("Start");
+		DataHandler dh = new DataHandler("/home/ivan/11.scp");
+		System.out.println(dh.getOInfo().getManufacturer());
+		System.out.println(dh.getOInfo().getPOSCP());
+		System.out.println(dh.getOInfo().getSerialNumber());
+		System.out.println(dh.getOInfo().getVersionPO());
+		System.out.println(dh.getOInfo().getPrint());
+		System.out.println(dh.getOInfo().getAnalysis());
+		System.out.println(dh.getOInfo().getReceive());
+		System.out.println(dh.getOInfo().getStorage());
+		System.out.println(dh.getOInfo().getAcquiringInstitutionDescription());
+		System.out.println(dh.getOInfo().getAnalyzingInstitutionDescription());
+		System.out.println(dh.getOInfo().getAcquiringDepartmentDescription());
+		System.out.println(dh.getOInfo().getAnalyzingDepartmentDescription());
+		System.out.println(dh.getOInfo().getLatestConfirmingPhysician());
+		System.out.println(dh.getOInfo().getReferringPhysician());
+		System.out.println(dh.getOInfo().getTechnicianDescription());
+		System.out.println(dh.getOInfo().getFreeTextField());
+		System.out.println(dh.getOInfo().getTimeOfAcquisition());
+		System.out.println(dh.getOInfo().getDateOfAcquisition());
+
+		System.out.println("End");
 	}
 }

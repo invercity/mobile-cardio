6ffabfa61090b03f7e325f39e7860fab18cff075
metlaivan@gmail.com <metlaivan@gmail.com>
2012-12-12 22:41:14 +0200

    fix some mistake in open with

--- SCPView/src/ua/stu/view/scpview/SCPViewActivity.java
+++ SCPView/src/ua/stu/view/scpview/SCPViewActivity.java
@@ -1,6 +1,5 @@
 package ua.stu.view.scpview;
 
-
 import group.pals.android.lib.ui.filechooser.FileChooserActivity;
 import group.pals.android.lib.ui.filechooser.io.localfile.LocalFile;
 import group.pals.android.lib.ui.filechooser.services.IFileProvider;
@@ -29,19 +28,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.webkit.MimeTypeMap;
 
-public class SCPViewActivity extends Activity implements OnEventItemClickListener,OnEventImageButtonClickListener
-{
+public class SCPViewActivity extends Activity implements
+		OnEventItemClickListener, OnEventImageButtonClickListener {
 	private static final String TAG = "SCPViewActivity";
 	private static final int REQUEST_CHOOSE_FILE = 0;
 	private static final String ROOT_PATH = "/mnt/sdcard";
-	
+
 	private ECGPanelFragment ecgPanel;
 	private InfoFragment info;
 	private FileChooserFragment fileChooser;
 	private DataHandler h;
 	/**
 	 * Current page in ViewPager.
-	 * <p><b>Default</b> 1 - ECGPanel
+	 * <p>
+	 * <b>Default</b> 1 - ECGPanel
 	 */
 	private int currentPage = 1;
 	/**
@@ -68,240 +68,234 @@ public class SCPViewActivity extends Activity implements OnEventItemClickListene
 	private Bundle state;
 	private ViewPager viewPager;
 	private GraphicView graphicView;
-    /** Called when the activity is first created. */
-	@Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-		Log.d(TAG,"onCreate");
-		setTheme(R.style.Theme_Sherlock);
-        super.onCreate(savedInstanceState);
-        
-        filePathKey = getResources().getString(R.string.app_file_path);
-        currentPageKey = getResources().getString(R.string.app_current_page);
-        
-        state = savedInstanceState;
-		
-		graphicView=new GraphicView(this);
-    }
-	@Override
-	public void onStart()
-	{
-		super.onStart();
-		Log.d(TAG,"onStart");
-		final android.content.Intent intent = getIntent ();
 
-	      if (intent != null)
-	      {
-	         final android.net.Uri data = intent.getData ();
-	         if (data != null)
-	         {	
-	           filePath = data.getEncodedPath ();
-	            System.out.println(filePath);	        
-	            // file loading comes here.
-	         } // if
-	      } // if
-	      if (state == null & filePath==null){
-	        	runFileChooser(R.style.Theme_Sherlock,ROOT_PATH);
-	        }
-	}
-	public String get_mime_by_filename(String filename){
-		  String ext;
-		  String type;
-		 
-		  int lastdot = filename.lastIndexOf(".");
-		  if(lastdot > 0){
-		    ext = filename.substring(lastdot + 1);
-		    MimeTypeMap mime = MimeTypeMap.getSingleton();
-		    type = mime.getMimeTypeFromExtension(ext);
-		    if(type != null) {
-		      return type;
-		    }
-		  }
-		 // return "application/octet-stream";
-		  return "no type";
-		}
+	/** Called when the activity is first created. */
 	@Override
-	public void onResume()
-	{
+	public void onCreate(Bundle savedInstanceState) {
+		Log.d(TAG, "onCreate");
+		setTheme(R.style.Theme_Sherlock);
+		super.onCreate(savedInstanceState);
+
+		filePathKey = getResources().getString(R.string.app_file_path);
+		currentPageKey = getResources().getString(R.string.app_current_page);
+
+		state = savedInstanceState;
+		graphicView = new GraphicView(this);
+		final android.content.Intent intent = getIntent();
+
+		if (intent != null) {
+			final android.net.Uri data = intent.getData();
+			if (data != null) {
+				filePath = data.getEncodedPath();
+				// file loading comes here.
+			} // if
+		} // if
+		if (state == null & filePath == null) {
+			runFileChooser(R.style.Theme_Sherlock, ROOT_PATH);
+		}
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+		Log.d(TAG, "onStart");
+
+	}
+//don't delete
+/*	public String get_mime_by_filename(String filename) {
+		String ext;
+		String type;
+
+		int lastdot = filename.lastIndexOf(".");
+		if (lastdot > 0) {
+			ext = filename.substring(lastdot + 1);
+			MimeTypeMap mime = MimeTypeMap.getSingleton();
+			type = mime.getMimeTypeFromExtension(ext);
+			if (type != null) {
+				return type;
+			}
+		}
+		// return "application/octet-stream";
+		return "no type";
+	}*/
+
+	@Override
+	public void onResume() {
 		super.onResume();
-		
-		try {			
-			System.out.println("------------");
-			System.out.println(filePath);
-			System.out.println(get_mime_by_filename(filePath));
-			System.out.println("-------------");
+
+		try {
 			h = new DataHandler(filePath);
-		}catch(Exception e){
-		  	Log.e(TAG, e.toString());
+		} catch (Exception e) {
+			Log.e(TAG, e.toString());
 		}
 
 		graphicView.setH(h);
 		ecgPanel = new ECGPanelFragment(graphicView);
-		info = new InfoFragment();		
+		info = new InfoFragment();
 		fileChooser = new FileChooserFragment();
-		
+
 		LayoutInflater inflater = LayoutInflater.from(this);
 		List<View> pages = new ArrayList<View>();
-		
-		View page = fileChooser.onCreateView(inflater,null,state);
+
+		View page = fileChooser.onCreateView(inflater, null, state);
 		pages.add(page);
-		  
-		page = ecgPanel.onCreateView(inflater,null,state);
+
+		page = ecgPanel.onCreateView(inflater, null, state);
 		pages.add(page);
-		  
-		page = info.onCreateView(inflater,null,state);
+
+		page = info.onCreateView(inflater, null, state);
 		pages.add(page);
-		      
+
 		viewPager = createPager(pages);
 		System.out.println(currentPage);
 		viewPager.setCurrentItem(currentPage);
-		
-		Log.d(TAG,"onResume");
+
+		Log.d(TAG, "onResume");
 	}
-	
+
 	@Override
-	public void onPause()
-	{
+	public void onPause() {
 		super.onPause();
-		Log.d(TAG,"onPause");
+		Log.d(TAG, "onPause");
 	}
-	
+
 	@Override
-	public void onStop()
-	{
+	public void onStop() {
 		super.onStop();
-		Log.d(TAG,"onStop");
+		Log.d(TAG, "onStop");
 	}
-	
+
 	@Override
-	public void onRestart()
-	{
+	public void onRestart() {
 		super.onRestart();
-		Log.d(TAG,"onRestart");
+		Log.d(TAG, "onRestart");
 	}
+
 	@Override
-	protected void onSaveInstanceState(Bundle outState) 
-	{
-	    super.onSaveInstanceState(outState);
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
 
-	    //save the current ecg file path
-	    outState.putString(filePathKey, filePath);
-	    //save the current page into viewPager
-	    outState.putInt(currentPageKey, viewPager.getCurrentItem());
+		// save the current ecg file path
+		outState.putString(filePathKey, filePath);
+		// save the current page into viewPager
+		outState.putInt(currentPageKey, viewPager.getCurrentItem());
 
-	    Log.d(TAG, "onSaveInstanceState");
+		Log.d(TAG, "onSaveInstanceState");
 	}
+
 	@Override
-	protected void onRestoreInstanceState(Bundle savedInstanceState) 
-	{
-	    super.onRestoreInstanceState(savedInstanceState);
+	protected void onRestoreInstanceState(Bundle savedInstanceState) {
+		super.onRestoreInstanceState(savedInstanceState);
 
-	    //restore the current ecg file path
-	    filePath = savedInstanceState.getString(filePathKey);
-	    //restore the current page into viewPager
-	    currentPage = savedInstanceState.getInt(currentPageKey);
+		// restore the current ecg file path
+		filePath = savedInstanceState.getString(filePathKey);
+		// restore the current page into viewPager
+		currentPage = savedInstanceState.getInt(currentPageKey);
 
-	    Log.d(TAG, "onRestoreInstanceState");
+		Log.d(TAG, "onRestoreInstanceState");
 	}
-	
+
 	/**
 	 * Method is handle which list item clicked on the InfoFragment
+	 * 
 	 * @param position
-	 * <p>0 - item for patient information</p>
-	 * <p>1 - item for other information</p>
+	 *            <p>
+	 *            0 - item for patient information
+	 *            </p>
+	 *            <p>
+	 *            1 - item for other information
+	 *            </p>
 	 */
 	@Override
 	public void itemClickEvent(int position) {
-		switch (position)
-		{
+		switch (position) {
 		case 0:
-			Hashtable<String, InfoP> patientTable = new Hashtable<String,InfoP>();
+			Hashtable<String, InfoP> patientTable = new Hashtable<String, InfoP>();
 			InfoP infoP = new InfoP(h.getPInfo().getAllPInfo());
 			patientKey = getResources().getString(R.string.app_patient);
-			patientTable.put(patientKey,infoP);
+			patientTable.put(patientKey, infoP);
 			try {
-				Intent intent = new Intent(this,PatientInfo.class);
-				intent.putExtra(patientKey,patientTable);
+				Intent intent = new Intent(this, PatientInfo.class);
+				intent.putExtra(patientKey, patientTable);
 				startActivity(intent);
-			}catch(Exception e){
-				Log.e("Error in A " , e.toString());
-			} 
+			} catch (Exception e) {
+				Log.e("Error in A ", e.toString());
+			}
 			break;
 		case 1:
-			Hashtable<String, InfoO> otherTable = new Hashtable<String,InfoO>();
+			Hashtable<String, InfoO> otherTable = new Hashtable<String, InfoO>();
 			InfoO infoO = new InfoO(h.getOInfo().getAllOInfo());
 			otherKey = getResources().getString(R.string.app_other);
 			otherTable.put(otherKey, infoO);
 			try {
-				Intent intent = new Intent(this,OtherInfo.class);
-				intent.putExtra(otherKey,otherTable);
+				Intent intent = new Intent(this, OtherInfo.class);
+				intent.putExtra(otherKey, otherTable);
 				startActivity(intent);
-			}catch(Exception e){
-				Log.e("Error in A " , e.toString());
+			} catch (Exception e) {
+				Log.e("Error in A ", e.toString());
 			}
 			break;
 		}
 	}
-	
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data){
-	    switch (requestCode)
-	    {
-	    case REQUEST_CHOOSE_FILE:
-	        if (resultCode == RESULT_OK) {
-	            /*
-	             * you can use two flags included in data
-	             */
-	            IFileProvider.FilterMode filterMode = (IFileProvider.FilterMode)
-	                data.getSerializableExtra(FileChooserActivity._FilterMode);
-	            boolean saveDialog = data.getBooleanExtra(FileChooserActivity._SaveDialog, false);
 
-	            /*
-	             * a list of files will always return,
-	             * if selection mode is single, the list contains one file
-	             */
-	            List<LocalFile> files = (List<LocalFile>)
-	                data.getSerializableExtra(FileChooserActivity._Results);
-	            
-	            filePath = files.get(0).getPath();
-	        }
-	        break;
-	    }
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		switch (requestCode) {
+		case REQUEST_CHOOSE_FILE:
+			if (resultCode == RESULT_OK) {
+				/*
+				 * you can use two flags included in data
+				 */
+				IFileProvider.FilterMode filterMode = (IFileProvider.FilterMode) data
+						.getSerializableExtra(FileChooserActivity._FilterMode);
+				boolean saveDialog = data.getBooleanExtra(
+						FileChooserActivity._SaveDialog, false);
+
+				/*
+				 * a list of files will always return, if selection mode is
+				 * single, the list contains one file
+				 */
+				List<LocalFile> files = (List<LocalFile>) data
+						.getSerializableExtra(FileChooserActivity._Results);
+
+				filePath = files.get(0).getPath();
+			}
+			break;
+		}
 	}
 
 	@Override
 	public void imageButtonClickEvent(int resId) {
 		switch (resId) {
 		case R.id.file_chooser:
-			Log.d(TAG,"file chooser");
-			runFileChooser(R.style.Theme_Sherlock,ROOT_PATH);
+			Log.d(TAG, "file chooser");
+			runFileChooser(R.style.Theme_Sherlock, ROOT_PATH);
 			break;
 		case R.id.camera:
-			Log.d(TAG,"camera");
+			Log.d(TAG, "camera");
 			break;
 		}
 	}
-	
-	private final void runFileChooser(int style,String rootPath)
-	{
+
+	private final void runFileChooser(int style, String rootPath) {
 		Intent intent = new Intent(getBaseContext(), FileChooserActivity.class);
 		/*
-		 * by default, if not specified, default rootpath is sdcard,
-		 * if sdcard is not available, "/" will be used
+		 * by default, if not specified, default rootpath is sdcard, if sdcard
+		 * is not available, "/" will be used
 		 */
 		intent.putExtra(FileChooserActivity._Theme, style);
-		intent.putExtra(FileChooserActivity._Rootpath, (Parcelable) new LocalFile(rootPath));
+		intent.putExtra(FileChooserActivity._Rootpath,
+				(Parcelable) new LocalFile(rootPath));
 		startActivityForResult(intent, REQUEST_CHOOSE_FILE);
 	}
-	
-	private final ViewPager createPager(List<View> pages)
-	{
+
+	private final ViewPager createPager(List<View> pages) {
 		SamplePagerAdapter pagerAdapter = new SamplePagerAdapter(pages);
-        ViewPager viewPager = new ViewPager(this);
-        viewPager.setAdapter(pagerAdapter);
-        viewPager.setCurrentItem(currentPage);     
-        
-        setContentView(viewPager);
-        return viewPager;
+		ViewPager viewPager = new ViewPager(this);
+		viewPager.setAdapter(pagerAdapter);
+		viewPager.setCurrentItem(currentPage);
+
+		setContentView(viewPager);
+		return viewPager;
 	}
 }
